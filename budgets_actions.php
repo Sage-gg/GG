fea9d3da7ef5907f5ef6e46948a33d2e67423749
LhayYhan <lhayyhan@gmail.com>
2026-01-31 22:17:43 +0800

    Update budgets_actions.php

--- budgets_actions.php
+++ budgets_actions.php
@@ -2,6 +2,8 @@
 // budgets_actions.php - UPDATED WITH AUTO-SYNC TO CORE 2
 session_start();
 include 'db.php';
+require_once 'core2_api_config.php';
+require_once 'core2_api_sender.php';
 
 function back($type, $msg){
   $_SESSION['flash'] = ['type'=>$type, 'msg'=>$msg];
@@ -196,4 +198,5 @@ function syncBudgetToCore2($budget_id) {
     'message' => 'Failed after ' . $max_attempts . ' attempts'
   ];
 }
+
 
